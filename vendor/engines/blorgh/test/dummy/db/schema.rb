a36ccac5feb9df4c1a88debe149285be4fbd47b3
Alex <lyosha85dev@gmail.com>
2015-06-08 11:58:36 +0900

    add comments table and migrate the engine

--- vendor/engines/blorgh/test/dummy/db/schema.rb
+++ vendor/engines/blorgh/test/dummy/db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608021523) do
+ActiveRecord::Schema.define(version: 20150608025744) do
 
   create_table "blorgh_articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "blorgh_comments", force: :cascade do |t|
+    t.integer  "article_id"
+    t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
